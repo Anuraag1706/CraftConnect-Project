4e577494ab00b60be865f045b3ce1902f8b6bc9c
Anuraag1706 <anuraag.karmakar1706@gmail.com>
2024-10-24 19:15:33 +0530

    Feedback updated

--- frontend/src/cutomer/Pages/HomePage/Feedback.js
+++ frontend/src/cutomer/Pages/HomePage/Feedback.js
@@ -12,23 +12,23 @@ function Feedback(){
       alignItems: 'center',
       minHeight: '100vh'
     }}>
-        <div class="wrapper"> 
-  <div class="title">Rate your experience</div>
-  <div class="content">We highly value your feedback! Kindly take a moment to rate your experience and provide us with your valuable feedback.</div>
-  <div class="rate-box">
+        <div className="wrapper"> 
+  <div className="title">Rate your experience</div>
+  <div className="content">We highly value your feedback! Kindly take a moment to rate your experience and provide us with your valuable feedback.</div>
+  <div className="rate-box">
     <input type="radio" name="star" id="star0"/>
-    <label class="star" for="star0"></label>
+    <label className="star" for="star0"></label>
     <input type="radio" name="star" id="star1"/>
-    <label class="star" for="star1"></label>
+    <label className="star" for="star1"></label>
     <input type="radio" name="star" id="star2" checked="checked"/>
-    <label class="star" for="star2"></label>
+    <label className="star" for="star2"></label>
     <input type="radio" name="star" id="star3"/>
-    <label class="star" for="star3"></label>
+    <label className="star" for="star3"></label>
     <input type="radio" name="star" id="star4"/>
-    <label class="star" for="star4"></label>
+    <label className="star" for="star4"></label>
   </div>
   <textarea cols="30" rows="6" placeholder="Tell us about your experience!"></textarea>
-  <div class="submit-btn">Send</div>
+  <div className="submit-btn">Send</div>
 </div>
       
     </div>
